8e4c023c4afebcb561389ac60241e5a4c583ef19
Kosuke-Takebayashi <82430348+Kosuke-Takebayashi@users.noreply.github.com>
2024-01-08 16:11:34 +0900

    ヘッダナビゲーションをメニュー化

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ function my_theme_set_up()
 {
 	add_theme_support('post-thumbnails'); // アイキャッチ画像を有効
 	add_theme_support('title-tag'); // titleタグを自動出力
-	register_nav_menu('main-menu', 'メインメニュー'); // カスタムメニューを作成
+	register_nav_menu('main-menu', 'header navigation'); // カスタムメニューを作成
 }
 add_action('after_setup_theme', 'my_theme_set_up');
 
